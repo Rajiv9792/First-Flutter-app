f4693f944bc0ca2f67afffa96137125e25e65013
Rajiv Sahu <rajivs9792@gmail.com>
2022-06-14 15:11:36 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+//Here we used widget trees to beautify the code always use comma at
+//the end of parenthesis also use home: after first widget and child lateral
+// Center widget used for proper alignment
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Center(
+      child: Text('Hello world'),
+    ),
+  ));
+}
