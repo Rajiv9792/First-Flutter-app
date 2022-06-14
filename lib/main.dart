70a55538e2255626f8be62feb681b584ee909a1f
Rajiv Sahu <rajivs9792@gmail.com>
2022-06-14 16:39:32 +0530

    First commit in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text('Hello world'),
-    ),
-  ));
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text(' Are You Rich'),
+              backgroundColor: Colors.pink.shade500),
+          backgroundColor: Colors.yellow.shade200,
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg'),
+            ),
+          ))));
 }
