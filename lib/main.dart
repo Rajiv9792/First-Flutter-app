84e2caef59b349abf12009325aae21a70c8cdb20
Rajiv Sahu <rajivs9792@gmail.com>
2022-06-15 15:38:34 +0530

    Day2_Working with Assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-              title: const Text(' Are You Rich'),
-              backgroundColor: Colors.pink.shade500),
-          backgroundColor: Colors.yellow.shade200,
-          body: const Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg'),
-            ),
-          ))));
+        appBar: AppBar(
+            title: const Text(' Are You Rich'),
+            backgroundColor: Colors.pink.shade500),
+        backgroundColor: Colors.yellow.shade200,
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/Mobo.png'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
